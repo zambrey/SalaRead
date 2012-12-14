550c0ac4fbdc1eaed6ab7dc630db56338bf625cb
Nate Osborne <nosborne@gmail.com>
2012-12-14 15:56:10 -0500

    Add hover info and personal page link to first treemap
    Added hover info window and link to personal page for the 2011 by Title
    treemap

--- InfoViz/TreeMaps.js
+++ InfoViz/TreeMaps.js
@@ -1,3 +1,111 @@
+var query;
+var data;
+var message;
+
+/*
+function init() {
+  drawTreeMap();
+}*/
+
+var key1 = "0Aihk8d-ie1rgdFR4Z0x3YXJHOUhSdHVOY2cybFNXQ0E";
+var sheet;
+var querytext1;
+
+
+// make the tooltip div follow the mouse
+$(function () {
+    $('#2011-bytitle').mousemove(function (e) {
+        $('#tooltip').css({
+            left: e.pageX + 3,
+            top: e.pageY - 50
+        });
+    });
+});
+
+
+google.load("visualization", "1", {packages:["treemap"]});
+google.setOnLoadCallback(function() {drawTreeMap(key1)});
+
+// treemap options
+var options = {
+  "showTip":true,
+  "titleTextStyle":{"fontSize":16},
+  "fontColor":"#fff",
+  "midColor":"#EFE6DC",
+  "animation":{"duration":0},
+  "headerColor":"#666666",
+  "maxColor":"#6d9eeb",
+  "headerHeight":40,
+  "fontSize":"14",
+  "showScale":true,
+  "hAxis":{"title":"Horizontal axis title","useFormatFromData":true,"minValue":null,"viewWindow":{"min":null,"max":null},"maxValue":null},
+  "vAxes":[{"title":"Left vertical axis title","useFormatFromData":true,"minValue":null,"viewWindow":{"min":null,"max":null},"maxValue":null},{"useFormatFromData":true,"minValue":null,"viewWindow":{"min":null,"max":null},"maxValue":null}],
+  "title":"Treemap by Title",
+  "booleanRole":"certainty",
+  "maxDepth":1,
+  "annotations":{"domain":{"style":"line"}},
+  "showTooltips":false,
+  "minColor":"#c27ba0",
+  "width":960,
+  "height":400,
+  "state":{},
+  "view":{"columns":[0,{"label":"Title","properties":{"role":"annotation"},"sourceColumn":1},2,3]},
+  "chartType":"TreeMap",
+  "chartName":"Chart 1"
+  };
+
+function drawTreeMap(sheet) {
+    var queryText1 = sheet + "&transpose=0&headers=1&range=A1%3AD1066&gid=7&pub=1";
+    //queryText1 = encodeURIComponent(queryText1);
+    var query = new google.visualization.Query("http://spreadsheets.google.com/tq?key=" + queryText1);
+    //var query = new google.visualization.Query("http://spreadsheets.google.com/tq?key=" + key1 + "&transpose=0&headers=1&range=A1%3AD1066&gid=7&pub=1");
+
+    // Send the query with a callback function.
+    query.send(handleQueryResponse);
+  }
+
+  function handleQueryResponse(response) {
+    if (response.isError()) {
+      alert('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
+      return;
+    }
+
+    data = response.getDataTable();
+    
+    visualization = new google.visualization.TreeMap(document.getElementById('2011-bytitle'));
+    visualization.draw(data, options);
+    google.visualization.events.addListener(visualization, 'select', selectHandler);
+    // hover window
+    google.visualization.events.addListener(visualization, 'onmouseover', function (e) {
+        var name = data.getValue(e.row, 0);
+        // format currency
+        var salary = addCommas(parseFloat(data.getValue(e.row, 2).toFixed(2)));
+        
+        // populate the tooltip with data
+        $('#tooltipTopLine').html(name);
+        $('#tooltipBottomLine').html("$" + salary);
+        // show the tooltip
+        $('#tooltip').show();
+    });
+    google.visualization.events.addListener(visualization, 'onmouseout', function (e) {
+        // hide the tooltip
+        $('#tooltip').hide();
+    });
+}
+
+function selectHandler() {
+  var selection = visualization.getSelection();
+  var item = selection[0]
+  if(item.row > 8)
+    {
+      var name = data.getValue(item.row, 0);
+      sessionStorage.clear();
+      sessionStorage.setItem(name,'selectedPerson');
+      // Open in a pop-up window
+      window.open("Personal.html","Employee Details","status=1,width=650,height=400,resizable=1");
+    }
+  }
+
 
 function changeTreeMap(year){
 		$("a.button2008, a.button2009, a.button2010, a.button2011").css({
