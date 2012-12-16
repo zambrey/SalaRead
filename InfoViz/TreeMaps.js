f0669162af40573a0deccb92637077bd32603b21
Nate Osborne <nosborne@gmail.com>
2012-12-15 22:10:33 -0500

    update 2011 dept treemap
    add tooltips and link to personal page for 2011 dept map
    fix autocomplete data javascript error

--- InfoViz/TreeMaps.js
+++ InfoViz/TreeMaps.js
@@ -1,11 +1,15 @@
-var query;
 var title2011Data;
+var dept2011Data;
+var title2010Data;
+var dept2010Data;
+var title2009Data;
+var dept2009Data;
+var title2008Data;
+var dept2008Data;
 
-var key1 = "0Aihk8d-ie1rgdFR4Z0x3YXJHOUhSdHVOY2cybFNXQ0E";
-var querytext1;
 
 var title2011Key = "0Aihk8d-ie1rgdFR4Z0x3YXJHOUhSdHVOY2cybFNXQ0E&transpose=0&headers=1&range=A1%3AD1066&gid=7&pub=1";
-var dept2011Key;
+var dept2011Key = "0Aihk8d-ie1rgdGNmWGw2YnVrZXFaaExwbmdvS3liN1E&transpose=0&headers=1&range=A1%3AD1099&gid=5&pub=1";
 var title2010Key;
 var dept2010Key;
 var title2009Key;
@@ -21,18 +25,39 @@ $(function () {
             top: e.pageY - 50
         });
     });
+    $('#2011-bydept').mousemove(function (e) {
+        $('#tooltip-dept2011').css({
+            left: e.pageX + 3,
+            top: e.pageY - 50
+        });
+    });
+    
 });
 
 google.load("visualization", "1", {packages:["treemap"]});
+google.setOnLoadCallback(function() {drawTreeMaps()});
 //google.setOnLoadCallback(function() {drawTreeMap(key1)});
-google.setOnLoadCallback(function() {drawMaps(title2011Key)});
+/*google.setOnLoadCallback(function() {drawMaps(title2011Key,dept2011Key)});
 
-function drawMaps(title2011) {
-  drawTreeMap(title2011);
+function drawMaps(title2011,dept2011) {
+  drawTitle2011Map(title2011);
+  drawDept2011Map(dept2011);
+//  drawTreeMap(dept2011);
+}*/
+
+function drawTreeMaps(){
+    var query1 = new google.visualization.Query("http://spreadsheets.google.com/tq?key=" + title2011Key);
+    // Send the query with a callback function.
+    query1.send(title2011Callback);
+    
+    var query2 = new google.visualization.Query("http://spreadsheets.google.com/tq?key=" + dept2011Key);
+    // Send the query with a callback function.
+    query2.send(dept2011Callback);
 }
 
+
 // treemap options
-var options = {
+var titleOptions = {
   "showTip":true,
   "titleTextStyle":{"fontSize":16},
   "fontColor":"#fff",
@@ -59,27 +84,57 @@ var options = {
   "chartName":"Chart 1"
   };
 
-function drawTreeMap(sheet) {
-    //var queryText1 = sheet + "&transpose=0&headers=1&range=A1%3AD1066&gid=7&pub=1";
-    //queryText1 = encodeURIComponent(queryText1);
-    var query = new google.visualization.Query("http://spreadsheets.google.com/tq?key=" + sheet);
-    //var query = new google.visualization.Query("http://spreadsheets.google.com/tq?key=" + key1 + "&transpose=0&headers=1&range=A1%3AD1066&gid=7&pub=1");
+var deptOptions = {
+    "showTip":true,
+    "titleTextStyle":{"fontSize":16},
+    "fontColor":"#fff",
+    "midColor":"#EFE6DC",
+    "animation":{"duration":0},
+    "headerColor":"#666666",
+    "maxColor":"#6d9eeb",
+    "headerHeight":40,
+    "fontSize":"14",
+    "showScale":true,
+    "hAxis":{"title":"Horizontal axis title","useFormatFromData":true,"minValue":null,"viewWindow":{"min":null,"max":null},"maxValue":null},
+    "vAxes":[{"title":"Left vertical axis title","useFormatFromData":true,"minValue":null,"viewWindow":{"min":null,"max":null},"maxValue":null},{"useFormatFromData":true,"minValue":null,"viewWindow":{"min":null,"max":null},"maxValue":null}],
+    "title":"Treemap by Department",
+    "booleanRole":"certainty",
+    "maxDepth":1,
+    "annotations":{"domain":{"style":"line"}},
+    "showTooltips":false,
+    "minColor":"#c27ba0",
+    "width":960,
+    "height":400,
+    "state":{},
+    "view":{"columns":[0,{"label":"Title","properties":{"role":"annotation"},"sourceColumn":1},2,3]},
+    "chartType":"TreeMap",
+    "chartName":"Chart 1"
+    };
 
+function drawTitle2011Map(key) {
+    var query = new google.visualization.Query("http://spreadsheets.google.com/tq?key=" + key);
     // Send the query with a callback function.
-    query.send(handleQueryResponse);
-  }
+    query.send(title2011Callback);
+}
+  
+function drawDept2011Map(key) {
+    var query = new google.visualization.Query("http://spreadsheets.google.com/tq?key=" + key);
+    // Send the query with a callback function.
+    query.send(dept2011Callback);
+}
 
-  function handleQueryResponse(response) {
+function title2011Callback(response) {
     if (response.isError()) {
       alert('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
       return;
     }
 
-    title2011data = response.getDataTable();
+    title2011Data = response.getDataTable();
     
     visualization = new google.visualization.TreeMap(document.getElementById('2011-bytitle'));
-    visualization.draw(title2011data, options);
+    visualization.draw(title2011Data, titleOptions);
     google.visualization.events.addListener(visualization, 'select', selectHandler);
+    // Go up button
     document.getElementById('goUp').disabled = true;
     document.getElementById('goUp').onclick = function () {
       visualization.goUpAndDraw();
@@ -87,9 +142,9 @@ function drawTreeMap(sheet) {
     };
     // hover window
     google.visualization.events.addListener(visualization, 'onmouseover', function (e) {
-        var name = title2011data.getValue(e.row, 0);
+        var name = title2011Data.getValue(e.row, 0);
         // format currency
-        var salary = addCommas(parseFloat(title2011data.getValue(e.row, 2).toFixed(2)));
+        var salary = addCommas(parseFloat(title2011Data.getValue(e.row, 2).toFixed(2)));
         
         // populate the tooltip with data
         $('#tooltipTopLine').html(name);
@@ -103,12 +158,48 @@ function drawTreeMap(sheet) {
     });
 }
 
+function dept2011Callback(response) {
+    if (response.isError()) {
+      alert('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
+      return;
+    }
+
+    dept2011Data = response.getDataTable();
+    
+    dept2011 = new google.visualization.TreeMap(document.getElementById('2011-bydept'));
+    dept2011.draw(dept2011Data, deptOptions);
+    google.visualization.events.addListener(dept2011, 'select', deptSelectHandler);
+    // Go up button
+    document.getElementById('goUp-dept2011').disabled = true;
+    document.getElementById('goUp-dept2011').onclick = function () {
+      dept2011.goUpAndDraw();
+      document.getElementById('goUp-dept2011').disabled = true;
+    };
+    
+    // hover window
+    google.visualization.events.addListener(dept2011, 'onmouseover', function (e) {
+        var name = dept2011Data.getValue(e.row, 0);
+        // format currency
+        var salary = addCommas(parseFloat(dept2011Data.getValue(e.row, 2).toFixed(2)));
+        
+        // populate the tooltip with data
+        $('#tooltipTopLine-dept2011').html(name);
+        $('#tooltipBottomLine-dept2011').html("$" + salary);
+        // show the tooltip
+        $('#tooltip-dept2011').show();
+    });
+    google.visualization.events.addListener(dept2011, 'onmouseout', function (e) {
+        // hide the tooltip
+        $('#tooltip-dept2011').hide();
+    });
+}
+
 function selectHandler() {
   var selection = visualization.getSelection();
   var item = selection[0]
   if(item.row > 8)
     {
-      var name = title2011data.getValue(item.row, 0);
+      var name = title2011Data.getValue(item.row, 0);
       sessionStorage.clear();
       sessionStorage.setItem(name,'selectedPerson');
       // Open in a pop-up window
@@ -121,6 +212,23 @@ function selectHandler() {
     }
   }
 
+function deptSelectHandler() {
+  var selection = dept2011.getSelection();
+  var item = selection[0]
+  if(item.row > 41)
+    {
+      var name = dept2011Data.getValue(item.row, 0);
+      sessionStorage.clear();
+      sessionStorage.setItem(name,'selectedPerson');
+      // Open in a pop-up window
+      window.open("Personal.html","Employee Details","status=1,width=650,height=400,resizable=1");
+    }
+  // Don't enable button at root level  
+  else if (item.row > 0)
+    {
+      document.getElementById('goUp-dept2011').disabled = false;
+    }
+}
 
 function changeTreeMap(year){
 		$("a.button2008, a.button2009, a.button2010, a.button2011").css({
