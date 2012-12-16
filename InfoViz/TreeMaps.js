149ed24669fc17001d22867a256200b7aa0a21e8
Nate Osborne <nosborne@gmail.com>
2012-12-16 00:53:37 -0500

    All treemaps with tooltips
    Added treemaps and personal page links to all treemaps
    Go up buttons still need work

--- InfoViz/TreeMaps.js
+++ InfoViz/TreeMaps.js
@@ -52,36 +52,28 @@ google.setOnLoadCallback(function() {drawTreeMaps()});
 
 function drawTreeMaps(){
     var query1 = new google.visualization.Query("http://spreadsheets.google.com/tq?key=" + title2011Key);
-    // Send the query with a callback function.
     query1.send(title2011Callback);
     
     var query2 = new google.visualization.Query("http://spreadsheets.google.com/tq?key=" + dept2011Key);
-    // Send the query with a callback function.
     query2.send(dept2011Callback);
 
-/*    var query3 = new google.visualization.Query("http://spreadsheets.google.com/tq?key=" + title2010Key);
-    // Send the query with a callback function.
+    var query3 = new google.visualization.Query("http://spreadsheets.google.com/tq?key=" + title2010Key);
     query3.send(title2010Callback);
     
     var query4 = new google.visualization.Query("http://spreadsheets.google.com/tq?key=" + dept2010Key);
-    // Send the query with a callback function.
     query4.send(dept2010Callback);
 
     var query5 = new google.visualization.Query("http://spreadsheets.google.com/tq?key=" + title2009Key);
-    // Send the query with a callback function.
     query5.send(title2009Callback);
     
     var query6 = new google.visualization.Query("http://spreadsheets.google.com/tq?key=" + dept2009Key);
-    // Send the query with a callback function.
     query6.send(dept2009Callback);
 
     var query7 = new google.visualization.Query("http://spreadsheets.google.com/tq?key=" + title2008Key);
-    // Send the query with a callback function.
     query7.send(title2008Callback);
     
     var query8 = new google.visualization.Query("http://spreadsheets.google.com/tq?key=" + dept2008Key);
-    // Send the query with a callback function.
-    query8.send(dept2008Callback); */
+    query8.send(dept2008Callback); 
 }
 
 // treemap options
@@ -147,7 +139,7 @@ function title2011Callback(response) {
     title2011Data = response.getDataTable();
     title2011 = new google.visualization.TreeMap(document.getElementById('2011-bytitle'));
     title2011.draw(title2011Data, titleOptions);
-    google.visualization.events.addListener(title2011, 'select', function () {titleSelectHandler (title2011,title2011Data,'title2011')});
+    google.visualization.events.addListener(title2011, 'select', function () {titleSelectHandler (title2011,title2011Data,title2011Div)});
     // Go up button
     document.getElementById('goUp-' + title2011Div).disabled = true;
     document.getElementById('goUp-' + title2011Div).onclick = function () {
@@ -163,13 +155,10 @@ function dept2011Callback(response) {
       alert('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
       return;
     }
-
     dept2011Data = response.getDataTable();
-    
     dept2011 = new google.visualization.TreeMap(document.getElementById('2011-bydept'));
     dept2011.draw(dept2011Data, deptOptions);
-//    google.visualization.events.addListener(dept2011, 'select', deptSelectHandler);
-    google.visualization.events.addListener(dept2011, 'select', deptSelectHandler);
+    google.visualization.events.addListener(dept2011, 'select', function () {deptSelectHandler (dept2011,dept2011Data,dept2011Div)});
     // Go up button
     document.getElementById('goUp-dept2011').disabled = true;
     document.getElementById('goUp-dept2011').onclick = function () {
@@ -179,23 +168,116 @@ function dept2011Callback(response) {
     addTooltip (dept2011,dept2011Data,'dept2011');
 }
 
-function selectHandler() {
-  var selection = visualization.getSelection();
-  var item = selection[0]
-  if(item.row > 8)
-    {
-      var name = title2011Data.getValue(item.row, 0);
-      sessionStorage.clear();
-      sessionStorage.setItem(name,'selectedPerson');
-      // Open in a pop-up window
-      window.open("Personal.html","Employee Details","status=1,width=650,height=400,resizable=1");
+function title2010Callback(response) {
+    if (response.isError()) {
+      alert('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
+      return;
     }
-  // Don't enable button at root level  
-  else if (item.row > 0)
-    {
-      document.getElementById('goUp').disabled = false;
+    title2010Data = response.getDataTable();
+    title2010 = new google.visualization.TreeMap(document.getElementById('2010-bytitle'));
+    title2010.draw(title2010Data, titleOptions);
+    google.visualization.events.addListener(title2010, 'select', function () {titleSelectHandler (title2010,title2010Data,title2010Div)});
+    // Go up button
+    document.getElementById('goUp-' + title2010Div).disabled = true;
+    document.getElementById('goUp-' + title2010Div).onclick = function () {
+      title2010.goUpAndDraw();
+      document.getElementById('goUp-' + title2010Div).disabled = true;
+    }; 
+    // hover window - visName, dataName, divSuffix
+    addTooltip (title2010,title2010Data,title2010Div);
+}
+
+function dept2010Callback(response) {
+    if (response.isError()) {
+      alert('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
+      return;
     }
-  }
+    dept2010Data = response.getDataTable();
+    dept2010 = new google.visualization.TreeMap(document.getElementById('2010-bydept'));
+    dept2010.draw(dept2010Data, deptOptions);
+    google.visualization.events.addListener(dept2010, 'select', function () {deptSelectHandler (dept2010,dept2010Data,dept2010Div)});
+    // Go up button
+    document.getElementById('goUp-dept2010').disabled = true;
+    document.getElementById('goUp-dept2010').onclick = function () {
+      dept2010.goUpAndDraw();
+      document.getElementById('goUp-dept2010').disabled = true;
+    };
+    addTooltip (dept2010,dept2010Data,'dept2010');
+}
+
+function title2009Callback(response) {
+    if (response.isError()) {
+      alert('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
+      return;
+    }
+    title2009Data = response.getDataTable();
+    title2009 = new google.visualization.TreeMap(document.getElementById('2009-bytitle'));
+    title2009.draw(title2009Data, titleOptions);
+    google.visualization.events.addListener(title2009, 'select', function () {titleSelectHandler (title2009,title2009Data,title2009Div)});
+    // Go up button
+    document.getElementById('goUp-' + title2009Div).disabled = true;
+    document.getElementById('goUp-' + title2009Div).onclick = function () {
+      title2009.goUpAndDraw();
+      document.getElementById('goUp-' + title2009Div).disabled = true;
+    }; 
+    // hover window - visName, dataName, divSuffix
+    addTooltip (title2009,title2009Data,title2009Div);
+}
+
+function dept2009Callback(response) {
+    if (response.isError()) {
+      alert('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
+      return;
+    }
+    dept2009Data = response.getDataTable();
+    dept2009 = new google.visualization.TreeMap(document.getElementById('2009-bydept'));
+    dept2009.draw(dept2009Data, deptOptions);
+    google.visualization.events.addListener(dept2009, 'select', function () {deptSelectHandler (dept2009,dept2009Data,dept2009Div)});
+    // Go up button
+    document.getElementById('goUp-dept2009').disabled = true;
+    document.getElementById('goUp-dept2009').onclick = function () {
+      dept2009.goUpAndDraw();
+      document.getElementById('goUp-dept2009').disabled = true;
+    };
+    addTooltip (dept2009,dept2009Data,'dept2009');
+}
+
+function title2008Callback(response) {
+    if (response.isError()) {
+      alert('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
+      return;
+    }
+    title2008Data = response.getDataTable();
+    title2008 = new google.visualization.TreeMap(document.getElementById('2008-bytitle'));
+    title2008.draw(title2008Data, titleOptions);
+    google.visualization.events.addListener(title2008, 'select', function () {titleSelectHandler (title2008,title2008Data,title2008Div)});
+    // Go up button
+    document.getElementById('goUp-' + title2008Div).disabled = true;
+    document.getElementById('goUp-' + title2008Div).onclick = function () {
+      title2008.goUpAndDraw();
+      document.getElementById('goUp-' + title2008Div).disabled = true;
+    }; 
+    // hover window - visName, dataName, divSuffix
+    addTooltip (title2008,title2008Data,title2008Div);
+}
+
+function dept2008Callback(response) {
+    if (response.isError()) {
+      alert('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
+      return;
+    }
+    dept2008Data = response.getDataTable();
+    dept2008 = new google.visualization.TreeMap(document.getElementById('2008-bydept'));
+    dept2008.draw(dept2008Data, deptOptions);
+    google.visualization.events.addListener(dept2008, 'select', function () {deptSelectHandler (dept2008,dept2008Data,dept2008Div)});
+    // Go up button
+    document.getElementById('goUp-dept2008').disabled = true;
+    document.getElementById('goUp-dept2008').onclick = function () {
+      dept2008.goUpAndDraw();
+      document.getElementById('goUp-dept2008').disabled = true;
+    };
+    addTooltip (dept2008,dept2008Data,'dept2008');
+}
 
 function titleSelectHandler(visualizationName,dataName,divSuffix) {
   var selection = visualizationName.getSelection();
@@ -215,12 +297,12 @@ function titleSelectHandler(visualizationName,dataName,divSuffix) {
     }
 }
 
-function deptSelectHandler() {
-  var selection = dept2011.getSelection();
+function deptSelectHandler(visualizationName,dataName,divSuffix) {
+  var selection = visualizationName.getSelection();
   var item = selection[0]
   if(item.row > 41)
     {
-      var name = dept2011Data.getValue(item.row, 0);
+      var name = dataName.getValue(item.row, 0);
       sessionStorage.clear();
       sessionStorage.setItem(name,'selectedPerson');
       // Open in a pop-up window
@@ -229,7 +311,7 @@ function deptSelectHandler() {
   // Don't enable button at root level  
   else if (item.row > 0)
     {
-      document.getElementById('goUp-dept2011').disabled = false;
+      document.getElementById('goUp-' + divSuffix).disabled = false;
     }
 }
 
